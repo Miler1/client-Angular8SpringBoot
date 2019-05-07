6a82bda510b7acb21b3944370d5012272f6ad237
Miler1 <milergrudtner@gmail.com>
2019-05-07 20:00:14 -0300

    updates to deploy to heroku

--- Angular8SpringBoot/src/assets/js/validador.js
+++ Angular8SpringBoot/src/assets/js/validador.js
@@ -1,12 +1,35 @@
-$(document).ready( function () {
+$(document).ready(function(){
+		$("#cpf").mask("999.999.999-99");
+  $("#salvar").click(function (e) {
+    var erros = [];
+    if ($('#nome').val() == "") {
+      erros.push("O campo nome é obrigatório.");
+    }
 
-  $('#salvar').on('click', function (e) {
-      // colocar o codigo d botao
-      alert();
-     
+    if ($('#cpf').val() == "") {
+      erros.push("O campo cpf é obrigatório.");
+    }
+    
+    if ($('#uf option:selected').text() == "") {
+      erros.push("O campo estado é obrigatório.");
+    }
+
+    var mensagem = "";
+    erros.forEach(function(value) {
+      console.log(value);
+      mensagem = mensagem +'\n'+ value;
+    });
+
+    console.log(mensagem);
+    console.log(erros);
+    Swal.fire({
+      type: 'error',
+      title: 'Oops...',
+      text: mensagem,
+    })
+
+    return false;
 
   });
-  
-  //$('#consultar_produtos, #consultar_usuarios').DataTable(); 
-
+ 
 });
