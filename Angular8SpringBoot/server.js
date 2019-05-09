2661bdd9ef7465285c897df3e071a3352400c121
Miler1 <milergrudtner@gmail.com>
2019-05-09 19:21:21 -0300

    Desenvolvimento do frontend em andamento

--- Angular8SpringBoot/server.js
+++ Angular8SpringBoot/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.set('port', (process.env.PORT || 4200));
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Angular8SpringBoot'));
@@ -13,4 +14,7 @@ res.sendFile(path.join(__dirname+'/dist/Angular8SpringBoot/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(app.get('port'), function() {
+  console.log('Node executando na porta ', app.get('port'));
+  console.log('Endereço: http://localhost:4200');
+});
